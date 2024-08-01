5594db8314eb6fae2ea73ad414a8badf500b8f8c
dmitry-podkyuko <d.podkuyko@nv.dunice.net>
2024-08-01 20:12:22 +0300

    fix: mv tasks 5

--- javascript_basic/5.js
+++ javascript_basic/5.js
@@ -1,13 +1,1 @@
-
-// Задача: Что будет выведено в консоль и почему?
-for (var i = 0; i < 3; i++) {
-  setTimeout(function () {
-    console.log(i);
-  }, 100);
-}
-
-for (let j = 0; j < 3; j++) {
-  setTimeout(function () {
-    console.log(j);
-  }, 100);
-}
+// TODO свободный слот для модератора
